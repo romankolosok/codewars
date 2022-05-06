7e8a45962971d24bda89d45ff9e479ff7185eaf6
kolosok.roman <romakolosok14@gmail.com>
2022-05-05 23:15:30 -0400

    solved lowercase counter

---
+++ 5.05/lowercasecount.js
@@ -0,0 +1,8 @@
+//Regex count lowercase letters
+function lowercaseCount(str){
+    let count = 0;
+    for(let i = 0; i < str.length; i++){
+      if(str[i] >= 'a' && str[i] <= 'z') count++
+    }
+    return count;
+  }
