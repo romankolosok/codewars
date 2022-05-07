250760a67e223e2fae0d1ed527af34db8cecbc9b
kolosok.roman <romakolosok14@gmail.com>
2022-05-06 23:19:30 -0400

    fixed bug in coin change kata

--- 6.05/NoOneLikesChange.js
+++ 6.05/NoOneLikesChange.js
@@ -1,6 +1,7 @@
 //No one likes carrying change.
 function coinsNeeded(amount, coin) {
     let count = 0;
+    coin.sort((a,b) => a - b);
     for(let i = coin.length-1; i >= 0; i--){
       count += Math.floor(amount/coin[i]);
       amount -= coin[i]*Math.floor(amount/coin[i]);
